1c8ca24e1675c31e592aad889857bb68b58e9b97
Lin Knudsen <onlineknudsen@gmail.com>
2020-12-24 11:44:13 -0800

    Implement underline text in Display class

--- src/Hardware/Display.cpp
+++ src/Hardware/Display.cpp
@@ -8,13 +8,50 @@ Display::Display(int csPin) : mx_(HARDWARE_TYPE, csPin, MAX_DEVICES) {
     memset(displayBuffer_, 0, DISPLAY_WIDTH);
 }
 
+void Display::printMsgCenterUnderline(const char* msg, int startChar, int endChar) {
+    byte length = strlen(msg);
+
+    byte charBuffer[8] = { 0 };
+    byte rawBuffer[DISPLAY_WIDTH] = { 0 };
+
+    byte msgWidth = 0;
+
+    int startCol = 0;
+    int endCol = 0;
+    for(int i = 0; i < length; i++) {
+        if(i == startChar) {
+            startCol = msgWidth - 1;
+        }
+        byte charWidth = mx_.getChar(msg[i], 8, charBuffer);
+        memcpy(rawBuffer + msgWidth, charBuffer, charWidth);
+        msgWidth += charWidth + CHAR_SPACING;
+        if(i == endChar) {
+            endCol = msgWidth;
+        }
+    }
+
+    byte padding = (DISPLAY_WIDTH - msgWidth + 1) / 2;
+    startCol += padding;
+    if(startCol < 0) {
+        startCol = 0;
+    }
+
+    endCol += padding;
+    if(endCol > DISPLAY_WIDTH - 1) {
+        endCol = DISPLAY_WIDTH - 1;
+    }
+
+    memcpy(displayBuffer_ + padding, rawBuffer, msgWidth);
+    underline(startCol, endCol);
+}
+
 void Display::printMsgCenter(const char* msg) {
    byte length = strlen(msg);
    byte charBuffer[8] = { 0 };
    byte rawBuffer[DISPLAY_WIDTH] = { 0 };
 
    byte msgWidth = 0;
-   for(byte i = 0; i < length; i++) {
+   for(int i = 0; i < length; i++) {
        byte charWidth = mx_.getChar(msg[i], 8, charBuffer);
        memcpy(rawBuffer + msgWidth, charBuffer, charWidth);
        msgWidth += charWidth + CHAR_SPACING;
@@ -22,6 +59,12 @@ void Display::printMsgCenter(const char* msg) {
 
    byte padding = (DISPLAY_WIDTH - msgWidth + 1) / 2;
    memcpy(displayBuffer_ + padding, rawBuffer, msgWidth);
+}
+
+void Display::underline(int startCol, int endCol) {
+    for(int i = startCol; i < endCol; i++) {
+        displayBuffer_[i] |= 0b10000000;
+    }
 }
 
 void Display::update() {
