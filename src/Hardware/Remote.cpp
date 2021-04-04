37c242cc14a8fb208b9e04c0b967ef67afe34438
Lin Knudsen <onlineknudsen@gmail.com>
2021-04-04 10:19:55 -0700

    Fix remote button

--- src/Hardware/Remote.cpp
+++ src/Hardware/Remote.cpp
@@ -1,15 +1,16 @@
 #include "Hardware/Remote.h"
 
-Remote::Remote() {
-    rfDriver_.init();
+Remote::Remote() : rfDriver_(2000, 34, 12) {
+    if(!rfDriver_.init()) {
+        Serial.println("Remote::Remote() - RF Driver failed to initialize");
+    }
 }
 
 bool Remote::receivedTurnOff() {
     byte buf[10];
     byte bufLen = sizeof(buf);
     if(rfDriver_.recv(buf, &bufLen)) {
-        char* msgReceived = (char*) buf;
-        return strcmp(msgReceived, "alarm_off") == 0;
+        return strcmp((char *) buf, "alarm_off") == 0;
     }
     return false;
 }
--- src/State/AlarmTriggeredState.cpp
+++ src/State/AlarmTriggeredState.cpp
@@ -7,7 +7,18 @@ void AlarmTriggeredState::update() {
     // play music
     hardware_.getAudioSource().loop();
     hardware_.getClock().getTimeStr(dateTimeStr_, 9);
-    if(millis() - timeout_ > 15000) {
+
+    if(isPrimaryAlarm_) {
+        // listen for snooze
+        // TODO: Change to snooze button
+        if(hardware_.getInput().checkDPadBtnJustPressed(Input::DPad::Up)) {
+            circadia_.setCurrentState(&circadia_.getClockState());
+        }
+    }
+
+    // listen for remote
+    if(hardware_.getRemote().receivedTurnOff()) {
+        remotePressed_ = true;
         circadia_.setCurrentState(&circadia_.getClockState());
     }
 }
